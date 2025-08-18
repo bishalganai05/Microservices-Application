802edd1bc72d0e9799e1b85ce7d87d0f0ea23729
Bishal Ganai <bishalganai05@gmail.com>
2025-08-18 09:18:28 +0530

    Initiated Book Service

---
+++ book-service/src/main/java/com/bishal/book/entity/Book.java
@@ -0,0 +1,19 @@
+package com.bishal.book.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	@Id
+	private Long bookId;
+	private String bookName;
+	private String issueDate;
+	private Long studentId;
+}
---
+++ book-service/src/test/java/com/bishal/book/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishal.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
